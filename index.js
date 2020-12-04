6ec2089e375d87762ea4d7881bd23b082c2cced2
uraki yuki <urakiyuki@urakinoMacBook-Pro.local>
2020-12-04 12:51:48 +0900

    create a new file

---
+++ index.js
@@ -0,0 +1,32 @@
+const { App } = require('@slack/bolt');
+const store = require('./store');
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN
+});
+
+
+app.event('channel_created', async ({ event, context }) => {
+  const channelCreatedNotifyChannel = process.env.CHANNEL_CREATED_NOTIFY_CHANNEL || '#general';
+  try {
+    const result = await app.client.chat.postMessage({
+      token: context.botToken,
+      channel: channelCreatedNotifyChannel,
+      text: `#${event.channel.name} チャンネルが作られたよ!🎉`,
+      link_names: true
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
+
+// Start your app
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ Bolt app is running!');
+})();
+
